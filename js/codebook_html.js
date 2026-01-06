aa8ead421bc59e2c69dde4667e50105b5c5533d7
John Liman <john@expeditionservices.com.au>
2026-01-07 08:38:16 +1100

    Fix the alignment in the button for REDCap v15

--- js/codebook_html.js
+++ js/codebook_html.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     // Check if button is found
     if (printButton.length > 0) {
         // Create new button
-        var newButton = $('<button class="jqbuttonmed hidden-when-popup ui-button ui-corner-all ui-widget" onclick="customDataDictionary()"><i class="fa-sharp fa-solid fa-book"></i> Custom Codebook</button>');
+        var newButton = $('<button class="hidden-when-popup ui-corner-all ui-widget" onclick="customDataDictionary()"><i class="fa-sharp fa-solid fa-book"></i> Custom Codebook</button>');
         // Append new button beside the found button
         printButton.after(newButton);
     }
